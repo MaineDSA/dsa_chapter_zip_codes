fba09b323288949055683cd843224cc11b36cbfe
Wil T <wil.t.me@pm.me>
2025-04-04 15:49:31 -0400

    format zeros with ruff

--- scraper/zeros.py
+++ scraper/zeros.py
@@ -3,17 +3,17 @@ import csv
 # maeve andersen
 # 19 january 2024
 # appends leading zeros to zips (dumb band-aid, fix this plz future me)
-file_path = 'chapter_zips.csv'
+file_path = "chapter_zips.csv"
 output_rows = []
 
-with open(file_path, 'r') as file:
+with open(file_path, "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
-        row['zip'] = row['zip'].zfill(5)
+        row["zip"] = row["zip"].zfill(5)
         output_rows.append(row)
 
-with open(file_path, 'w', newline='') as file:
-    fieldnames = ['zip', 'chapter']
+with open(file_path, "w", newline="") as file:
+    fieldnames = ["zip", "chapter"]
     writer = csv.DictWriter(file, fieldnames=fieldnames)
 
     # Write the header
